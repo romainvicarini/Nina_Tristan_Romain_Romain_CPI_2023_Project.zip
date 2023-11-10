ddf8ffb346225bb84cff9dafae391aad3557758c
Triton455 <150430131+Triton455@users.noreply.github.com>
2023-11-10 11:43:51 +0100

    Add files via upload

---
+++ fonction.py
@@ -0,0 +1,20 @@
+import random
+peter_DP_list=[3,4,5,6]
+mangos_DP_list=[1,2]
+
+def peterNormalDamage() :
+    peter_DP=random.choice(peter_DP_list)
+    return peter_DP
+
+def mangosNormalDamage():
+    mangos_DP=random.choice(mangos_DP_list)
+    return mangos_DP
+
+
+
+
+
+
+
+
+    
