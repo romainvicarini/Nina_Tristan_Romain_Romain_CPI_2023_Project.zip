594d6d296cae6f3e121c73420a3bcd0e21668bf6
Triton455 <150430131+Triton455@users.noreply.github.com>
2023-11-10 11:45:55 +0100

    Update fonction.py

--- fonction.py
+++ fonction.py
@@ -1,13 +1,13 @@
 import random
-peter_DP_list=[3,4,5,6]
+peter_DP_list=[3,4,5,6] 
 mangos_DP_list=[1,2]
 
 def peterNormalDamage() :
-    peter_DP=random.choice(peter_DP_list)
+    peter_DP=random.choice(peter_DP_list) #randomize the attack value of Peter
     return peter_DP
 
 def mangosNormalDamage():
-    mangos_DP=random.choice(mangos_DP_list)
+    mangos_DP=random.choice(mangos_DP_list) ##randomize the attack value of mangos
     return mangos_DP
 
 
