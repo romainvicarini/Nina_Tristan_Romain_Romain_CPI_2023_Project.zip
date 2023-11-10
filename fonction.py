86df30b996cce5fde441a823cae4033960d6497d
Triton455 <150430131+Triton455@users.noreply.github.com>
2023-11-10 12:10:30 +0100

    Update fonction.py

--- fonction.py
+++ fonction.py
@@ -2,11 +2,11 @@ import random
 
 
 def peter_normal_damage() :
-    peter_DP=random.choice(peter_DP_list) #give the random attack value of Peter
+    peter_DP=random.choice(peter_DP_list) #give a random attack value of Peter
     return peter_DP
 
 def mangos_normal_damage():
-    mangos_DP=random.choice(mangos_DP_list) #give the random attack value of mangos
+    mangos_DP=random.choice(mangos_DP_list) #give a random attack value of mangos
     return mangos_DP
 
 print("Oh no! A mangoose is coming.\nStart of the fight, choose your attack:\nNormal attack: enter True\nSpecial attack: enter False")
