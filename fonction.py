331d128f7c165541a5ad47239a7ba36b39d5f25b
NinzLef <150430758+NinzLef@users.noreply.github.com>
2023-11-10 11:59:30 +0100

    Update fonction.py

--- fonction.py
+++ fonction.py
@@ -9,8 +9,12 @@ def mangosNormalDamage():
     mangos_DP=random.choice(mangos_DP_list) ##randomize the attack value of mangos
     return mangos_DP
 
-
-
+print("Oh no! A mangoose is coming.\nStart of the fight, choose your attack:\nNormal attack: enter True\nSpecial attack: enter False")
+def attack(n):
+    if n==True:
+        
+    if n==False:
+        #Nina is loading...
 
 
 
