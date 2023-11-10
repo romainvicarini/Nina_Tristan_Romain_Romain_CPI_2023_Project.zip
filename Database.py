0c3887b69dae69e5d760d91fbce30aade256e182
Boulvert <150429874+Boulvert@users.noreply.github.com>
2023-11-10 11:37:52 +0100

    Add files via upload

--- Database.py
+++ Database.py
@@ -1,9 +1,32 @@
-min_DP=3
+peter_min_DP=3
 
-max_DP=6
+peter_max_DP=6
+
+peter_DP=0
+
+peter_DP_list=[]
 
 peter_HP=20
 
+peter_armor_HP=1
+
 mangos_HP=10
 
+mangos_min_DP=1
+
+mangos_max_DP=1
+
+mangos_DP=0
+
+mangos_DP_list=[]
+
 bestScores=[]
+
+lunchbox=True
+
+
+
+
+
+
+
