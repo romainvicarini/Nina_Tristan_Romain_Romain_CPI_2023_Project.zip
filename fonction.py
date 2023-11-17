7a623b7de701ceb307c332b6210d7c0c7e2f91a9
NinzLef <150430758+NinzLef@users.noreply.github.com>
2023-11-17 09:23:36 +0100

    Update fonction.py

--- fonction.py
+++ fonction.py
@@ -14,7 +14,18 @@ def attack(n):
     if n==True:
         #Tristan is loading... 
     if n==False:
-        #Nina is loading...
+        print("Choose your special attack: \n Defend: enter 1 \n Snake eyes: enter 2 \n Last chance: enter 3 \n If you want to go back to regular attack: enter True")
+            def specialattack(n):
+                if n==1: #defend = armorPV *2 + regular attack
+                    peter_armor_HP = peter_armor_HP*2
+                    if peter_HP%2==0:
+                        #see regular attack
+                if n==2: snake eyes #regular attack + DP inflicted = DP gained => fonction 'bin'
+                    #see regular attack
+                    peter_HP = peter_HP + peter_DP
+                if n==3: last chance #DP max *3 but sometimes miss the target
+                        lastchance = False
+                        mangos_hp = mangos_hp-???*3 #hits for maximum damage multiplied by 3
 
 
 
